96c1876f6566e88b17ac545341dfaee61c414e92
sazo <sazdev09@gmail.com>
2023-11-12 19:13:30 +0100

    add the logic of the game to the file motor

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 import {partida } from "./modelo";
+import { generarCarta, generarValorCarta, obtenerMensajeSegunPuntos } from "./motor"
 
 const divPuntuacion = document.getElementById("puntuacion");
 const btnPlantarse= document.getElementById("plantarse");
@@ -89,36 +90,6 @@ const actualizarInterfazJugadaFutura = (): void => {
     titulo.textContent = "Esta seria tu siguiente jugada";
   } else {
     console.error("No se ha encontrado el elemento titulo");
-  }
-};
-
-const generarNumeroAleatorio = (): number => {
-  return Math.floor(Math.random() * 10) + 1;
-};
-
-const generarCarta = (): number => {
-  partida.numeroAleatorio = generarNumeroAleatorio();
-  return partida.numeroAleatorio > 7 ? partida.numeroAleatorio + 2 : partida.numeroAleatorio;
-};
-
-const generarValorCarta = (valorCarta: number): number => {
-  partida.puntos = valorCarta >= 10 ? 0.5 : valorCarta;
-  return partida.puntos;
-};
-
-const obtenerMensajeSegunPuntos = (puntos: number): string => {
-  if (puntos === 0) {
-    return "Juego de las siete media";
-  } else if (puntos < 4) {
-    return "Has sido muy conservador";
-  } else if (puntos >= 4 && puntos < 6) {
-    return "Te ha entrado el canguelo eh?";
-  } else if (puntos >= 6 && puntos <= 7) {
-    return "Casi casi...";
-  } else if (puntos === 7.5) {
-    return "¡Lo has clavado! ¡Enhorabuena!";
-  } else {
-    return "Game Over";
   }
 };
 
